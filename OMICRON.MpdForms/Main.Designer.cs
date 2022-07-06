0a7f3135acd2b6a895e096958801adcebdb2ba5f
dev <dev@dev.org>
2022-07-06 16:42:52 +0200

    Projektdateien hinzufügen.

---
+++ OMICRON.MpdForms/Main.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace OMICRON.MpdForms
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxVrms = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxQiec = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxVrms
+            // 
+            this.textBoxVrms.Location = new System.Drawing.Point(217, 12);
+            this.textBoxVrms.Name = "textBoxVrms";
+            this.textBoxVrms.Size = new System.Drawing.Size(571, 22);
+            this.textBoxVrms.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "V rms (measured value in mV)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Q IEC (measured value in pC)";
+            // 
+            // textBoxQiec
+            // 
+            this.textBoxQiec.Location = new System.Drawing.Point(217, 52);
+            this.textBoxQiec.Name = "textBoxQiec";
+            this.textBoxQiec.Size = new System.Drawing.Size(571, 22);
+            this.textBoxQiec.TabIndex = 3;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxQiec);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxVrms);
+            this.Name = "Main";
+            this.Text = "MPD540";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxVrms;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxQiec;
+    }
+}
+
---
+++ OMICRON.MpdForms/Main.cs
@@ -0,0 +1,48 @@
+﻿using mtronix;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Threading;
+
+namespace OMICRON.MpdForms
+{
+    public partial class Main : Form, IConsumer2
+    {
+        readonly double qIecHandle;
+        readonly double vRmsHandle;
+        public Dispatcher Dispatcher { get; set; }
+        public Main()
+        {
+            InitializeComponent();
+
+            var app = new mtronixMeasurementApp();
+            qIecHandle = app.registerConsumer2("station1.qIec", "MPD540", this);
+            vRmsHandle = app.registerConsumer2("station1.vRms", "MPD540", this);
+            Dispatcher = Dispatcher.CurrentDispatcher;
+        }
+
+        public void notifyValueEvent(int handle, object v)
+        {
+            
+            var value = (double)v;
+            if (handle == qIecHandle)
+            {
+                Dispatcher.Invoke(new Action(() => {
+                    textBoxQiec.Text = (value * 1000000000000.000000).ToString();
+                }), null);
+            } 
+            if(handle == vRmsHandle)
+            {
+                Dispatcher.Invoke(new Action(() => {
+                    textBoxVrms.Text = (value * 1000.000000).ToString();
+                }), null);
+            }
+        }
+    }
+}
